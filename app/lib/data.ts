dd9304f65db9c5e268efc5c0ce84dde0a1106040
zahraaz <zahraamahmood222@gmail.com>
2024-01-17 10:00:54 +0300

    adding data

---
+++ app/lib/data.ts
@@ -0,0 +1,52 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Child Vaccination Management",
+    descriptions:
+      "An information system to track children's periodic vaccination schedules, coordinating with parents. Designed for Al-Quds Medical Centre, focusing on organized vaccination scheduling",
+    stack: ["ReactJS", "MUI", "NodeJS", "ExpressJS", "MySQL", "TypeORM"],
+  },
+  {
+    title: "Dilmun",
+    descriptions:
+      "Dilmun is a website that allows people to sell, buy, and even donate products with a click of a button. With its availability in 3 different languages, it also helps people to meet and connect online. By signing up with either your email, Google, or Facebook account, you'll get to see the world we built in Dilmun.",
+    stack: [
+      "ReactJS",
+      "Redux Toolkit",
+      "TailwindCSS",
+      "Firebase",
+      "Headless UI",
+      "Farmer Motion",
+    ],
+  },
+  {
+    title: "FashionEgo ",
+    descriptions:
+      "An e-commerce website for women's clothing designers and customers.Enabled designers to showcase their work, and customers to reserve and order designs",
+    stack: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "ReactJS",
+      "MUI",
+      "NodeJS",
+      "ExpressJS",
+      "MySQL",
+      "TypeORM",
+    ],
+  },
+];
