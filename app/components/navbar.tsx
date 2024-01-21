2b05061befbab324582f66520bc7f7600e2dc75d
zahraaz <zahraamahmood222@gmail.com>
2024-01-21 23:17:30 +0300

    adding navbar menu

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,7 +5,7 @@ import photo from "@/public/photo.jpg";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-opacity-90 ">
       <div className="flex flex-wrap items-center justify-between mx-auto p-8">
         <Link href="/">
           <Image
@@ -17,7 +17,35 @@ export default function Navbar() {
             className="inline-block h-12 w-12 rounded-full ring-2"
           />
         </Link>
-        <div className="menu"></div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="font-medium flex p-4 md:p-0  rounded-lg flex-row md:space-x-8 mt-0">
+            <li>
+              {" "}
+              <Link
+                href="#about"
+                className="block py-2 pl-3 pr-4 text-sky-900 sm:text-xl rounded md:p-0 hover:text-white"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#projects"
+                className="block py-2 pl-3 pr-4 text-sky-900 sm:text-xl rounded md:p-0 hover:text-white"
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#contact"
+                className="block py-2 pl-3 pr-4 text-sky-900 sm:text-xl rounded md:p-0 hover:text-white"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
