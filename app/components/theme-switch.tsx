4f52dc00e462fb683cfcf13f5e5936c93ac797e7
zahraaz <zahraamahmood222@gmail.com>
2024-01-24 12:04:26 +0300

    saving dark mode value to the local storage

--- app/components/theme-switch.tsx
+++ app/components/theme-switch.tsx
@@ -1,6 +1,29 @@
-import { Sun } from "react-feather";
+"use client";
+import { useState, useEffect } from "react";
+import { Sun, Moon } from "react-feather";
+
+type Theme = "light" | "dark";
 
 export default function ThemeSwitch() {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+    if (localTheme) {
+      setTheme(localTheme);
+    }
+  }, []);
+
   return (
     <button
       className="fixed bottom-5 right-5  w-[3rem] h-[3rem] 
@@ -8,8 +31,9 @@ export default function ThemeSwitch() {
       rounded-full flex items-center justify-center
       border border-white border-opacity-40 bg-white
       hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
     >
-      <Sun />
+      {theme === "light" ? <Sun /> : <Moon />}
     </button>
   );
 }
