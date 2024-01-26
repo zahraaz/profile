ef3024741eb5810ebff34df05bcefcd949000a63
zahraaz <zahraamahmood222@gmail.com>
2024-01-26 14:13:36 +0300

    updating footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -8,23 +8,24 @@ export default function Footer() {
       className="m-0 px-4 text-center text-gray-500
     dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90"
     >
-      <small className=" m-0 flex justify-center">
+      <small className="flex justify-center  my-0 mx-8">
         <Link href="https://github.com/zahraaz">
           <GitHub />
         </Link>
         <Link href="https://www.linkedin.com/in/zahraa-mahmood/">
           <Linkedin />
         </Link>
-        <Link href="https://www.instagram.com/zahraa_mahmod.dev/">
-          <Instagram />
-        </Link>
+
         <Link href="https://web.facebook.com/zahra.chan.3003/">
           <Facebook />
+        </Link>
+        <Link href="https://www.instagram.com/zahraa_mahmod.dev/">
+          <Instagram />
         </Link>
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span>
-        Made with <Heart className="inline size-4" /> & verious tech in Mosul,
+        Made with <Heart className="inline size-3" /> & various tech in Mosul,
         Iraq &copy; Zahraa Mahmood 2024
       </p>
     </footer>
